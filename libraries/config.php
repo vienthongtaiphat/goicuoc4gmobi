f1188252e3ca4590adc0bea3761f80d704afaadc
vienthongtaiphat <151997422+vienthongtaiphat@users.noreply.github.com>
2024-01-03 15:23:27 +0700

    Update config.php

--- libraries/config.php
+++ libraries/config.php
@@ -1,216 +1,92 @@
 <?php
-
-if (!defined('LIBRARIES')) die("Error");
-
-/* Root */
-
-define('ROOT', __DIR__);
-
-/* Timezone */
-
-date_default_timezone_set('Asia/Ho_Chi_Minh');
-
-/* Cấu hình coder */
-
-define('NN_MSHD', '1438322w');
-
-define('NN_AUTHOR', 'vienthongtaiphat.it@gmail.com');
-
-/* Cấu hình chung */
-
-$config = array(
-
-	/*'author' => array(
-
-		'name' => 'Đỗ Quang Vinh',
-
-		'email' => 'vienthongtaiphat.it@gmail.com',
-
-		'timefinish' => date('m/Y', time() - (86400 * 30))
-
-	),*/
-
-	'arrayDomainSSL' => array(),
-
-	'database' => array(
-
-		//'server-name' => $_SERVER["SERVER_NAME"],
-
-		'url' => '/',
-
-		'type' => 'mysql',
-
-		'host' => 'localhost',
-
-		'username' => 'qpuukjvaeh',
-
-		'password' => 'gS9PfCDttq',
-
-		'dbname' => 'qpuukjvaeh',
-
-		'port' => 3306,
-
-		'prefix' => 'table_',
-
-		'charset' => 'utf8'
-
-	),
-
-	'website' => array(
-
-		'error-reporting' => false,
-
-		'secret' => '@nina',
-
-		'salt' => 'l7MPb0U0L8',
-
-		'debug-developer' => false,
-
-		'debug-css' => false,
-
-		'debug-js' => true,
-
-		'index' => false,
-
-		'upload' => array(
-
-			'max-width' => 1600,
-
-			'max-height' => 1600
-
-		),
-
-		'lang' => array(
-
-			'vi' => 'Tiếng Việt',
-
-			// 'en'=>'Tiếng Anh',
-
-			// 'cn'=>'Tiếng Trung',
-
-			// 'ko'=>'Tiếng Hàn',
-
-			// 'jp'=>'Tiếng Nhật',
-
-			// 'kh'=>'Tiếng Campuchia',
-
-		),
-
-		'lang-doc' => 'vi',
-
-		'slug' => array(
-
-			'vi' => 'Tiếng Việt',
-
-		),
-
-		'seo' => array(
-
-			'vi' => 'Tiếng Việt',
-
-			// 'en'=>'Tiếng Anh',
-
-			// 'cn'=>'Tiếng Trung',
-
-			// 'ko'=>'Tiếng Hàn',
-
-			// 'jp'=>'Tiếng Nhật',
-
-			// 'kh'=>'Tiếng Campuchia',
-
-		),
-
-		'comlang' => array()
-
-	),
-
-	'order' => array(
-
-		'ship' => false
-
-	),
-
-	'login' => array(
-
-		'admin' => 'LoginAdmin' . NN_MSHD,
-
-		'member' => 'LoginMember' . NN_MSHD,
-
-		'attempt' => 5,
-
-		'delay' => 15
-
-	),
-
-	'googleAPI' => array(
-
-		'recaptcha' => array(
-
-			'active' => false,
-
-			'urlapi' => 'https://www.google.com/recaptcha/api/siteverify',
-
-			'sitekey' => '6LezS5kUAAAAAF2A6ICaSvm7R5M-BUAcVOgJT_31',
-
-			'secretkey' => '6LezS5kUAAAAAGCGtfV7C1DyiqlPFFuxvacuJfdq'
-
-		)
-
-	),
-
-	'oneSignal' => array(
-
-		'active' => false,
-
-		'id' => 'af12ae0e-cfb7-41d0-91d8-8997fca889f8',
-
-		'restId' => 'MWFmZGVhMzYtY2U0Zi00MjA0LTg0ODEtZWFkZTZlNmM1MDg4'
-
-	),
-
-	'license' => array(
-
-		'version' => "",
-
-		'powered' => ""
-
-	)
-
-);
-
-//if ($_SERVER["SERVER_NAME"] == 'goicuoc4gmobifone.com') $config['key'] = '2f3931106578ed4b67ca90a6997d0e4b';
-
-//$config['pattern'] = '111';
-
-/* Error reporting */
-
-error_reporting(($config['website']['error-reporting']) ? E_ALL : 0);
-
-/* Cấu hình base */
-
-$http = 'http';
-
-if ($_SERVER["HTTPS"] == "on") {
-
-	$http .= "s";
-
-}
-
-$http .= "://";
-
-$config_url = $config['database']['server-name'] . $config['database']['url'];
-
-$config_base = $http . $config_url;
-
-/* Cấu hình login */
-
-$login_admin = $config['login']['admin'];
-
-$login_member = $config['login']['member'];
-
-/* Cấu hình upload */
-
-require_once LIBRARIES . "constant.php";
-
-if ($config['arrayDomainSSL']) require_once LIBRARIES . "checkSSL.php";
-
+define( 'WP_CACHE', true ); 
+/**
+ * The base configurations of the WordPress.
+ *
+ * This file has the following configurations: MySQL settings, Table Prefix,
+ * Secret Keys, WordPress Language, and ABSPATH. You can find more information
+ * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
+ * wp-config.php} Codex page. You can get the MySQL settings from your web host.
+ *
+ * This file is used by the wp-config.php creation script during the
+ * installation. You don't have to use the web site, you can just copy this file
+ * to "wp-config.php" and fill in the values.
+ *
+ * @package WordPress
+ */
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'qpuukjvaeh');
+/** MySQL database username */
+define('DB_USER', 'qpuukjvaeh');
+/** MySQL database password */
+define('DB_PASSWORD', 'gS9PfCDttq');
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+/**#@+
+ * Authentication Unique Keys and Salts.
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ */
+require('wp-salt.php');
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each a unique
+ * prefix. Only numbers, letters, and underscores please!
+ */
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each a unique
+ * prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'table_';
+/**
+ * WordPress Localized Language, defaults to English.
+ *
+ * Change this to localize WordPress. A corresponding MO file for the chosen
+ * language must be installed to wp-content/languages. For example, install
+ * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
+ * language support.
+ */
+define('FS_METHOD','direct');
+define('WPLANG', '');
+define('FS_CHMOD_DIR', (0775 & ~ umask()));
+define('FS_CHMOD_FILE', (0664 & ~ umask()));
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ */
+define('WP_DEBUG', false);
+define( 'WP_REDIS_CONFIG', [
+   'token' => "e279430effe043b8c17d3f3c751c4c0846bc70c97f0eaaea766b4079001c",
+   'host' => '127.0.0.1',
+   'port' => 6379,
+   'database' => "4992", 
+   'timeout' => 2.5,
+   'read_timeout' => 2.5,
+   'split_alloptions' => true,
+   'async_flush' => true,
+   'client' => 'phpredis', 
+   'compression' => 'zstd', 
+   'serializer' => 'igbinary', 
+   'prefetch' => true, 
+   'debug' => false,
+   'save_commands' => false,
+   'prefix' => DB_NAME,  
+   ] );
+define( 'WP_REDIS_DISABLED', false );
+/* That's all, stop editing! Happy blogging. */
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+        define('ABSPATH', dirname(__FILE__) . '/');
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
